dcf32b0eae09ef8297ee82a8240bbeba4e2f8d65
jjung-ah <rurounida88@gmail.com>
2021-05-13 17:39:24 +0900

    Change train.py

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ def main():
 	images, labels = dataiter.next()
 
 	cfg = {  # 8 + 3 =11 == vgg11
-    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    # 10 + 3 = vgg 13
-    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    # 13 + 3 = vgg 16
-    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    # 16 +3 =vgg 19
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+	'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	# 10 + 3 = vgg 13
+	'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	# 13 + 3 = vgg 16
+	'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+	# 16 +3 =vgg 19
+	'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
 	  }
-    conv = make_layers(cfg[Configs.model_config], batch_norm=True)
-    model = Vgg(conv, num_classes=Configs.class_num, init_weights=True)
-    print(model)
+	conv = make_layers(cfg[Configs.model_config], batch_norm=True)
+	model = Vgg(conv, num_classes=Configs.class_num, init_weights=True)
+	print(model)
 
 	train(model, trainloader, testloader)
 
