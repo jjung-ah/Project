0e9a724e894ba69231a1603676e5a15c4cc31210
jjung-ah <rurounida88@gmail.com>
2021-05-26 15:05:56 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+# 테스트 데이터를 사용하여 모델을 테스트한다.
+with torch.no_grad(): # torch.no_grad()를 하면 gradient 계산을 수행하지 않는다.
+    X_test = mnist_test.test_data.view(-1, 28 * 28).float().to(device)
+    Y_test = mnist_test.test_labels.to(device)
+
+    prediction = linear(X_test)
+    correct_prediction = torch.argmax(prediction, 1) == Y_test
+    accuracy = correct_prediction.float().mean()
+    print('Accuracy:', accuracy.item())
+
+    # MNIST 테스트 데이터에서 무작위로 하나를 뽑아서 예측을 해본다
+    r = random.randint(0, len(mnist_test) - 1)
+    X_single_data = mnist_test.test_data[r:r + 1].view(-1, 28 * 28).float().to(device)
+    Y_single_data = mnist_test.test_labels[r:r + 1].to(device)
+
+    print('Label: ', Y_single_data.item())
+    single_prediction = linear(X_single_data)
+    print('Prediction: ', torch.argmax(single_prediction, 1).item())
+
+    plt.imshow(mnist_test.test_data[r:r + 1].view(28, 28), cmap='Greys', interpolation='nearest')
+    plt.show()
