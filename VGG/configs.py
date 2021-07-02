134900e582ceb90387234082d5c02d2166bea81d
jjung-ah <rurounida88@gmail.com>
2021-07-02 11:03:31 +0900

    Create TransferLearningModel folder

---
+++ VGG/configs.py
@@ -0,0 +1,14 @@
+class Configs:
+    # settings
+    data_root = '/content/gdrive/Shareddrives/zeogi_gogi/dataset/'
+
+    # processing data
+    #image_gray = True
+    #normalize = (0.5,) if image_gray else (0.5, 0.5, 0.5)
+    #split = True
+    #train_percent = 0.9
+    model_config = 'D'
+    class_num = 7
+    epochs_nb = 100
+    model_path = "/data/FoodDetection/Object_Detection/yolov5-test/ssd/models/vgg16_epoch10_batch393_accuracy73.223.pt"
+
---
+++ VGG/main.py
@@ -0,0 +1,39 @@
+from torchvision.utils import save_image
+from MyDataLoader import *
+from torchvision.models.vgg import make_layers
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from vggModule import VGG
+from vggModule import *
+from configs import Configs
+from train import train
+
+
+def main():
+    trainloader, testloader = make_train_val_set(Configs.data_root)
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    cfg = {  # 8 + 3 =11 == vgg11
+	    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	    # 10 + 3 = vgg 13
+	    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	    # 13 + 3 = vgg 16
+	    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+	    # 16 +3 =vgg 19
+	    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+	}
+    conv = make_layers(cfg[Configs.model_config], batch_norm=True)
+    model = VGG(conv, num_classes=Configs.class_num, init_weights=True)
+    print(model)
+
+    train(model, trainloader, testloader)
+
+
+
+if __name__ == '__main__':
+	
+    main()
