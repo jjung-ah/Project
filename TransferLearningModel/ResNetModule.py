69c1bc8297bd78802c3ebcaf61d260f518a5f475
jjung-ah <rurounida88@gmail.com>
2021-07-05 18:05:01 +0900

    Create ResNet file

---
+++ TransferLearningModel/ResNetModule.py
@@ -0,0 +1,84 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+from configs import Configs
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_block, num_classes=10, init_weights=True):
+        super().__init__()
+
+        self.in_channels=64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        # weights inittialization
+        if init_weights:
+            self._initialize_weights()
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self,x):
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        x = self.conv3_x(output)
+        x = self.conv4_x(x)
+        x = self.conv5_x(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    # define weight initialization function
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+def resnet18():
+    return ResNet(BasicBlock, [2,2,2,2])
+
+def resnet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+def resnet50():
+    return ResNet(BottleNeck, [3,4,6,3])
+
+def resnet101():
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+def resnet152():
+    return ResNet(BottleNeck, [3, 8, 36, 3])
+        
+        
